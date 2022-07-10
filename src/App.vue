a645f70baf49de17210fd5eafd9b0df516895cf0
Joao Pedro Fernandes <joao.pedro.fernandes.122@gmail.com>
2022-07-10 14:36:46 -0300

    starting the project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class ="columns is-gapless is-multiline">
+      <div class="column is-one-queter">
+
+      </div>
+
+      <div class="column is-three-queter">
+
+      </div>
+
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App'
+});
+</script>
+
+<style>
+
+</style>
